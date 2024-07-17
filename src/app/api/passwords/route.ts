4fcf735726f7483cadb0e0ba19848cbece1afe01
Junianto Wicaksono <juniantowicaksono06@gmail.com>
2024-07-17 13:48:26 +0700

    Debug

--- src/app/api/passwords/route.ts
+++ src/app/api/passwords/route.ts
@@ -76,6 +76,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
             });
         }
         const userDataJSON = req.headers.get('x-user-data') as string;
+        console.log(userDataJSON);
         const userData = JSON.parse(userDataJSON) as Forms.IPasswords;
         await ConnectDB();
         const passwordItem = new passwordsCollection({
@@ -100,6 +101,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
         
     }
     catch(error) {
+        console.error(error)
         return Response.json({
             code: 500,
             message: "Internal server error"
