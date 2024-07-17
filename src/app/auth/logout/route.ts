7b3f35d9ca0a5cb331ab8cf0ef1c3e26bf141227
Junianto Wicaksono <juniantowicaksono06@gmail.com>
2024-07-17 14:56:26 +0700

    Try Fix logout 2

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -2,30 +2,13 @@ import { cookies } from 'next/headers';
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest, res: NextResponse) {
-    const cookieStore = cookies();
-    cookies().delete('accessToken');
-    cookies().delete('refreshToken');
-    cookies().set('accessToken', '',{
-        expires: new Date('2016-10-05'),
-        path: '/',
-        secure: /^true$/i.test(process.env.USE_SECURE as string),
-        partitioned: /^true$/i.test(process.env.USE_SECURE as string),
-    });
-    cookies().set('refreshToken', '',{
-        expires: new Date('2016-10-05'),
-        path: '/',
-        secure: /^true$/i.test(process.env.USE_SECURE as string),
-        partitioned: /^true$/i.test(process.env.USE_SECURE as string),
-    });
-    console.log("LOGOUT")
-    cookieStore.getAll().forEach(cookie => {
-        console.log(cookie.name);
-        cookieStore.delete(cookie.name);
-    })
+    // const cookieStore = cookies();
     const response = NextResponse.redirect(new URL('/auth/login', process.env.APP_BASE_URL as string));
-    let cookieList = cookieStore.getAll();
-    cookieList.forEach(cookie => {
-        response.cookies.delete(cookie.name);
-    })
+    response.cookies.set('accessToken', '', { maxAge: 0 });
+    response.cookies.set('refreshToken', '', { maxAge: 0 });
+    // let cookieList = cookieStore.getAll();
+    // cookieList.forEach(cookie => {
+    //     response.cookies.delete(cookie.name);
+    // })
     return response;
 }
