088a8774587ab5b73a69075c038842538929d5f9
Junianto Wicaksono <juniantowicaksono06@gmail.com>
2024-07-17 14:33:27 +0700

    Fix logout

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -5,6 +5,21 @@ export async function GET(req: NextRequest, res: NextResponse) {
     const cookieStore = cookies();
     cookies().delete('accessToken');
     cookies().delete('refreshToken');
+    cookies().set({
+        name: 'accessToken',
+        value: '',
+        expires: new Date('2016-10-05'),
+        path: '/'
+    });
+    cookies().set({
+        name: 'refreshToken',
+        value: '',
+        expires: new Date('2016-10-05'),
+        path: '/'
+    });
+    cookieStore.getAll().forEach(cookie => {
+        cookieStore.delete(cookie.name);
+    })
     const response = NextResponse.redirect(new URL('/auth/login', process.env.APP_BASE_URL as string));
     let cookieList = cookieStore.getAll();
     cookieList.forEach(cookie => {
