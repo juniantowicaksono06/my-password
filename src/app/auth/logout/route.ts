89cd508ea9e19643f025bc0a52f1df8259d42202
Junianto Wicaksono <juniantowicaksono06@gmail.com>
2024-07-23 13:41:19 +0700

    Troubleshoot

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -4,7 +4,7 @@ import { NextResponse, type NextRequest } from 'next/server';
 export async function GET(req: NextRequest, res: NextResponse) {
     const cookieStore = cookies();
     const response = NextResponse.redirect(new URL('/auth/login', process.env.APP_BASE_URL as string));
-    
+    console.log("LOGOUT FROM LOGOUT BUTTON")
     cookies().set('accessToken', "", {
         httpOnly: true,
         secure: /^true$/i.test(process.env.USE_SECURE as string),
