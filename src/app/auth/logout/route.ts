81bbc333130ebbea6363677fd7593dc14014f22b
Junianto Wicaksono <juniantowicaksono06@gmail.com>
2024-07-23 13:57:51 +0700

    Troubleshoot Logout

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -1,33 +1,27 @@
 import { cookies } from 'next/headers';
 import { NextResponse, type NextRequest } from 'next/server';
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
     if (req.method !== 'GET') {
-        return NextResponse.error();
+        return NextResponse.json({ error: 'Method not allowed' }, { status: 405 });
     }
 
-    // Ensure this logic runs only on the client side
-    if (typeof window !== 'undefined') {
-        const cookieStore = cookies();
-        const response = NextResponse.redirect(new URL('/auth/login', process.env.APP_BASE_URL as string));
-        console.log("LOGOUT FROM LOGOUT BUTTON");
-        
-        cookies().set('accessToken', "", {
-            httpOnly: true,
-            secure: /^true$/i.test(process.env.USE_SECURE as string),
-            path: '/',
-            maxAge: -1
-        });
-        
-        cookies().set('refreshToken', "", {
-            httpOnly: true,
-            secure: /^true$/i.test(process.env.USE_SECURE as string),
-            path: '/',
-            maxAge: -1
-        });
-        
-        return response;
-    }
+    const response = NextResponse.redirect(new URL('/auth/login', process.env.APP_BASE_URL as string));
+    console.log("LOGOUT FROM LOGOUT BUTTON");
+    
+    cookies().set('accessToken', "", {
+        httpOnly: true,
+        secure: /^true$/i.test(process.env.USE_SECURE as string),
+        path: '/',
+        maxAge: -1
+    });
+    
+    cookies().set('refreshToken', "", {
+        httpOnly: true,
+        secure: /^true$/i.test(process.env.USE_SECURE as string),
+        path: '/',
+        maxAge: -1
+    });
 
-    return NextResponse.error(); // Return an error response if not running on the client side
+    return response;
 }
